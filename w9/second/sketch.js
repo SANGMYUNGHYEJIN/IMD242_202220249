73999791a9fcccb7b9b062745c01588891378964
SANGMYUNGHYEJIN <163003127+SANGMYUNGHYEJIN@users.noreply.github.com>
2024-11-11 18:01:02 +0900

    10

---
+++ w9/second/sketch.js
@@ -0,0 +1,31 @@
+let tileNumX = 16;
+let tileNumY = 12;
+
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  background('pink');
+  //background('#ff3300');
+  noStroke();
+  fill('cornflowerblue');
+  // for (let column = 0; column < width; column += 40) {
+  //   for (let row = 0; row < width; row += 40) {
+  //     let x = 20 + column;
+  //     let y = 20 + row;
+  //     let diameter = 30;
+  //     circle(x, y, diameter);
+  //   }
+  // }
+
+  for (let row = 0; row < tileNumY; row++) {
+    for (let column = 0; column < tileNumX; column++) {
+      let tileW = width / tileNumX;
+      let tileH = height / tileNumY;
+      let x = tileW * 0.5 + column * tileW;
+      let y = tileH * 0.5 + row * tileH;
+      ellipse(x, y, tileW, tileH);
+    }
+  }
+}
