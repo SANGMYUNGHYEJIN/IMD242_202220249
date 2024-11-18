9ba8cf8c37cf71337e7278e9ed1e8dd543eb50d7
SANGMYUNGHYEJIN <163003127+SANGMYUNGHYEJIN@users.noreply.github.com>
2024-11-19 00:02:38 +0900

    w10과제완료

--- w10/w10_hw/sketch.js
+++ w10/w10_hw/sketch.js
@@ -1,36 +1,33 @@
-const colours = [
-  #257180,
-  #F2E5BF,
-  #FD8B51,
-  #CB6040
-];
+const colours = ['#257180', '#F2E5BF', '#FD8B51', '#CB6040'];
 let gravity = [0, 0.1];
 let friction = 0.99;
 let cnt = 0;
 let mouse = [0, 0];
 
-ArrayList<Confetti> confetties;
+let confetties = [];
 
 function setup() {
-  size(800, 800);
-  confetties = new ArrayList<Confetti>();
+  createCanvas(800, 800);
 }
 
-void gen(float x, float y, float n) {
-  for (int idx = 0; idx < n; idx++) {
-    float randomW = random(4, 20);
-    float randomH = random(4, 20);
-    float randomForce = random(1, 10);
-    float randomAngForce = random(-30, 30);
-    Confetti newConfetti = new Confetti(x, y,
-      randomW, randomH,
+function gen(x, y, n) {
+  for (let idx = 0; idx < n; idx++) {
+    let randomW = random(4, 20);
+    let randomH = random(4, 20);
+    let randomForce = random(1, 10);
+    let randomAngForce = random(-30, 30);
+    let newConfetti = new Confetti(
+      x,
+      y,
+      randomW,
+      randomH,
       colours[int(random(colours.length))],
-      randomForce, randomAngForce);
-    confetties.add(newConfetti);
+      randomForce,
+      randomAngForce
+    );
+    confetties.push(newConfetti);
   }
 }
-
-
 
 function mousePressed() {
   cnt = 0;
@@ -39,27 +36,82 @@ function mousePressed() {
 }
 
 function mouseReleased() {
-  println("gen: " + cnt);
+  console.log('gen: ' + cnt);
   gen(mouse[0], mouse[1], cnt);
 }
 
 function keyPressed() {
-  if (key =='p' || key =='P')
-    println("confetties: " + confetties.size());
+  if (key === 'p' || key === 'P')
+    console.log('confetties: ' + confetties.length);
 }
 
 function draw() {
-  if (mousePressed) {
+  if (mouseIsPressed) {
     cnt++;
   }
   background(255);
-  for (let idx = confetties.size() - 1; idx >= 0; idx--) {
-    Confetti aConfetti = confetties.get(idx);
+  for (let idx = confetties.length - 1; idx >= 0; idx--) {
+    let aConfetti = confetties[idx];
     aConfetti.update(gravity, friction);
     if (aConfetti.isOutOfScreen()) {
-      confetties.remove(idx);
+      confetties.splice(idx, 1);
     }
   }
-  for (let idx = 0; idx < confetties.size(); idx++)
-    confetties.get(idx).display();
+  for (let idx = 0; idx < confetties.length; idx++) {
+    let aConfetti = confetties[idx];
+    if (aConfetti !== undefined) {
+      aConfetti.display();
+    }
+  }
+}
+
+class Confetti {
+  constructor(x, y, w, h, colour, force, angForce) {
+    this.pos = [x, y];
+    this.vel = [0, 0];
+    this.size = [w, h];
+    this.colour = colour;
+
+    let randomDir = radians(random(360));
+    this.vel[0] = force * cos(randomDir);
+    this.vel[1] = force * sin(randomDir);
+
+    this.ang = radians(random(360));
+    this.angVel = radians(angForce);
+  }
+  update(force, friction) {
+    for (let idx = 0; idx < 2; idx++) {
+      this.vel[idx] += force[idx];
+      this.pos[idx] += this.vel[idx];
+      this.vel[idx] *= friction;
+    }
+
+    this.ang += this.angVel;
+    this.angVel *= friction;
+  }
+
+  display() {
+    push();
+    rectMode(CENTER);
+    translate(this.pos[0], this.pos[1]);
+    rotate(this.ang);
+    noStroke();
+    fill(this.colour);
+    rect(0, 0, this.size[0], this.size[1]);
+    pop();
+  }
+
+  getDiagonal() {
+    let sumSquare = pow(this.size[0] * 0.5, 2) + pow(this.size[1] * 0.5, 2);
+    return sqrt(sumSquare);
+  }
+
+  isOutOfScreen() {
+    return (
+      this.pos[0] < -this.getDiagonal() ||
+      this.pos[0] > width + this.getDiagonal() ||
+      this.pos[1] < -this.getDiagonal() ||
+      this.pos[1] > height + this.getDiagonal()
+    );
+  }
 }
